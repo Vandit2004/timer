d4bfa12819501bfde60233479297cbdff408d7b0
Vandit2004 <149085334+Vandit2004@users.noreply.github.com>
2026-02-11 19:43:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+let display = document.querySelector(".time")
+let [seconds,minutes,hours]=[0,0,0];
+let timer = null
+
+function stopwatch(){
+    seconds++
+    if(seconds==60){
+        seconds = 0
+        minutes++
+        if(minutes == 60){
+            minutes = 0
+            hours++
+        }
+    }
+    let h = hours<10? "0" + hours : hours
+    let m = minutes<10? "0" + minutes : minutes
+    let s = seconds<10? "0" + seconds : seconds
+    display.innerHTML = h +":"+m+":"+s
+}
+function watchstart(){
+    if(timer!==null){
+        clearInterval(timer)
+    }
+    timer = setInterval(stopwatch,1000)
+}
+function watchstop(){
+    clearInterval(timer)
+}
+function watchreset(){
+    clearInterval(timer)
+    timer = null
+    seconds = 0
+    minutes = 0
+    hours = 0
+    display.innerHTML = "00:00:00"
+}
